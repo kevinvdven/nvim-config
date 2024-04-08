3ac52e23165a1267a2090d9f8fa570cd31f86405
Kevin van de Ven <kvandeven@turnitin.com>
2024-04-08 14:37:50 +0200

    Show hidden files with treesitter and ignore git and node_modules folders

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,21 @@ local M = {
   'nvim-telescope/telescope.nvim', tag = '0.1.6',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function ()
+    local telescope = require('telescope')
+    telescope.setup({
+      defaults = {
+        file_ignore_patterns = {
+          "git/",
+          "node_modules/"
+        }
+      },
+      pickers = {
+        find_files = {
+          hidden = true
+        }
+      }
+    })
+
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
